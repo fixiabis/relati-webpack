c823dee3e615a953ec17898fd800b0607bf5296b
fixiabis <benkao0000@gmail.com>
2018-07-15 15:35:56 +0800

    full rule

--- alpha/js/relati.js
+++ alpha/js/relati.js
@@ -4,37 +4,37 @@ var createSVG = (function () {
 })();
 
 class RelatiGame {
-    constructor(container) {
-        var board = new GridBoard(5, 5);
+    constructor(container, size) {
+        var board = new GridBoard(size, size);
         var viewer = createSVG("svg");
         var vLines = [];
         var hLines = [];
         var gridSize = (Math.min(
             container.clientWidth,
             container.clientHeight
-        ) * 0.8 / 5) | 0;
+        ) * 0.9 / size) | 0;
         var gridLineSize = gridSize / 10;
 
-        for (let x = 1; x < 5; x++) {
+        for (let x = 1; x < size; x++) {
             let line = createSVG("path");
             line.setAttribute("stroke-width", gridLineSize / 2);
-            line.setAttribute("d", `M ${x * gridSize} 0 V ${gridSize * 5}`);
+            line.setAttribute("d", `M ${x * gridSize} 0 V ${gridSize * size}`);
             line.setAttribute("stroke", "#888");
             viewer.appendChild(line);
             vLines.push(line);
         }
 
-        for (let y = 1; y < 5; y++) {
+        for (let y = 1; y < size; y++) {
             let line = createSVG("path");
             line.setAttribute("stroke-width", gridLineSize / 2);
-            line.setAttribute("d", `M 0 ${y * gridSize} H ${gridSize * 5}`)
+            line.setAttribute("d", `M 0 ${y * gridSize} H ${gridSize * size}`)
             line.setAttribute("stroke", "#888");
             viewer.appendChild(line);
             hLines.push(line);
         }
 
-        viewer.setAttribute("width", gridSize * 5);
-        viewer.setAttribute("height", gridSize * 5);
+        viewer.setAttribute("width", gridSize * size);
+        viewer.setAttribute("height", gridSize * size);
         viewer.addEventListener("click", function (event) {
             var x = (event.offsetX / this.gridSize) | 0,
                 y = (event.offsetY / this.gridSize) | 0;
@@ -44,6 +44,7 @@ class RelatiGame {
 
         container.appendChild(viewer);
 
+        this.size = size;
         this.board = board;
         this.viewer = viewer;
         this.vLines = vLines;
@@ -53,26 +54,27 @@ class RelatiGame {
     }
     viewerResize() {
         var container = this.container;
+        var size = this.size;
         var gridSize = (Math.min(
             container.clientWidth,
             container.clientHeight
-        ) * 0.8 / 5);
+        ) * 0.9 / size) | 0;
         var gridLineSize = gridSize / 10;
 
-        for (let x = 1; x < 5; x++) {
+        for (let x = 1; x < size; x++) {
             var line = this.vLines[x - 1];
             line.setAttribute("stroke-width", gridLineSize / 2);
-            line.setAttribute("d", `M ${x * gridSize} 0 V ${gridSize * 5}`);
+            line.setAttribute("d", `M ${x * gridSize} 0 V ${gridSize * size}`);
         }
 
-        for (let y = 1; y < 5; y++) {
+        for (let y = 1; y < size; y++) {
             var line = this.hLines[y - 1];
             line.setAttribute("stroke-width", gridLineSize / 2);
-            line.setAttribute("d", `M 0 ${y * gridSize} H ${gridSize * 5}`)
+            line.setAttribute("d", `M 0 ${y * gridSize} H ${gridSize * size}`)
         }
 
-        this.viewer.setAttribute("width", gridSize * 5);
-        this.viewer.setAttribute("height", gridSize * 5);
+        this.viewer.setAttribute("width", gridSize * size);
+        this.viewer.setAttribute("height", gridSize * size);
         this.gridSize = gridSize;
     }
 }
