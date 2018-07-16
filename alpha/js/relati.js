6ff4fedc02920c023e3a6e75427334978e177584
fixiabis <benkao0000@gmail.com>
2018-07-16 20:33:29 +0800

    new UI

--- alpha/js/relati.js
+++ alpha/js/relati.js
@@ -3,78 +3,97 @@ var createSVG = (function () {
     return tag => document.createElementNS(svgNS, tag);
 })();
 
-class RelatiGame {
-    constructor(container, size) {
-        var board = new GridBoard(size, size);
-        var viewer = createSVG("svg");
-        var vLines = [];
-        var hLines = [];
-        var gridSize = (Math.min(
-            container.clientWidth,
-            container.clientHeight
-        ) * 0.9 / size) | 0;
-        var gridLineSize = gridSize / 10;
+var getRelatiList = (function () {
+    var dirO = ["F", "B", "R", "L", "FR", "FL", "BR", "BL"]
+    var dir2O = dirO.map(dir => dir + dir);
+    var dirTX = ["FFR", "FFL", "BBR", "BBL", "FRR", "FLL", "BRR", "BLL"];
 
-        for (let x = 1; x < size; x++) {
-            let line = createSVG("path");
-            line.setAttribute("stroke-width", gridLineSize / 2);
-            line.setAttribute("d", `M ${x * gridSize} 0 V ${gridSize * size}`);
-            line.setAttribute("stroke", "#888");
-            viewer.appendChild(line);
-            vLines.push(line);
+    function normalRelatiList(grid, sym) {
+        var list = [];
+
+        for (var i = 0; i < dirO.length; i++) {
+            var sourceGrid = grid.getGridFromDir(dirO[i]);
+            if (!grid) continue;
+
+            if (sourceGrid.symbol === sym) {
+                list.push(grid);
+            }
         }
 
-        for (let y = 1; y < size; y++) {
-            let line = createSVG("path");
-            line.setAttribute("stroke-width", gridLineSize / 2);
-            line.setAttribute("d", `M 0 ${y * gridSize} H ${gridSize * size}`)
-            line.setAttribute("stroke", "#888");
-            viewer.appendChild(line);
-            hLines.push(line);
-        }
-
-        viewer.setAttribute("width", gridSize * size);
-        viewer.setAttribute("height", gridSize * size);
-        viewer.addEventListener("click", function (event) {
-            var x = (event.offsetX / this.gridSize) | 0,
-                y = (event.offsetY / this.gridSize) | 0;
-            var grid = board.grids[x][y];
-            if (board.ongridselect) board.ongridselect(grid);
-        }.bind(this));
-
-        container.appendChild(viewer);
-
-        this.size = size;
-        this.board = board;
-        this.viewer = viewer;
-        this.vLines = vLines;
-        this.hLines = hLines;
-        this.gridSize = gridSize;
-        this.container = container
+        return list;
     }
-    viewerResize() {
-        var container = this.container;
-        var size = this.size;
-        var gridSize = (Math.min(
-            container.clientWidth,
-            container.clientHeight
-        ) * 0.9 / size) | 0;
-        var gridLineSize = gridSize / 10;
 
-        for (let x = 1; x < size; x++) {
-            var line = this.vLines[x - 1];
-            line.setAttribute("stroke-width", gridLineSize / 2);
-            line.setAttribute("d", `M ${x * gridSize} 0 V ${gridSize * size}`);
+    function remoteRelatiList(grid, sym) {
+        var list = [];
+
+        for (var i = 0; i < dir2O.length; i++) {
+            var sourceGrid = grid.getGridFromDir(dir2O[i]);
+            if (!grid) continue;
+
+            var spaceGrid = grid.getGridFromDir(dirO[i]);
+
+            if (sourceGrid.symbol === sym && spaceGrid.symbol === "") {
+                list.push(grid);
+            }
         }
 
-        for (let y = 1; y < size; y++) {
-            var line = this.hLines[y - 1];
-            line.setAttribute("stroke-width", gridLineSize / 2);
-            line.setAttribute("d", `M 0 ${y * gridSize} H ${gridSize * size}`)
-        }
-
-        this.viewer.setAttribute("width", gridSize * size);
-        this.viewer.setAttribute("height", gridSize * size);
-        this.gridSize = gridSize;
+        return list;
     }
-}
+
+    function remoteStableRelatiList(grid, sym) {
+        var list = [];
+
+        for (var i = 0; i < dirTX.length; i++) {
+            var sourceGrid = grid.getGridFromDir(dirTX[i]);
+            if (!grid) continue;
+
+            var spaceGrid2T = grid.getGridFromDir(dir2O[(i / 2) | 0]);
+            var spaceGridT = grid.getGridFromDir(dirO[(i / 2) | 0]);
+            var spaceGridIH = grid.getGridFromDir(dirO[(i / 2) | 0 + 4]);
+            var spaceGridI = grid.getGridFromDir(dirO[(((i % 4) | 0) / 2) | 0]);
+            var spaceGridH = grid.getGridFromDir(dirO[i % 2 + 2]);
+
+            if (
+                sourceGrid.symbol === sym && (
+                    spaceGrid2T.symbol === "" &&
+                    spaceGridT.symbol === "" ||
+                    spaceGridIH.symbol === "" && (
+                        spaceGridI.symbol === "" ||
+                        spaceGridH.symbol === ""
+                    )
+                )
+            ) {
+                list.push(grid);
+            }
+        }
+
+        return list;
+    }
+
+    return function (grid, sym) {
+        var list = [];
+
+        list = list.concat(normalRelatiList(grid, sym));
+        list = list.concat(remoteRelatiList(grid, sym));
+        list = list.concat(remoteStableRelatiList(grid, sym));
+
+        return list;
+    }
+})();
+
+var gameStartButton = document.getElementById("game-start");
+var welcomePage = document.getElementById("welcome-page");
+var selectPage = document.getElementById("select-page");
+var board5x5 = document.getElementById("board-5x5");
+var board7x7 = document.getElementById("board-7x7");
+var board9x9 = document.getElementById("board-9x9");
+var board11x11 = document.getElementById("board-11x11")
+
+gameStartButton.addEventListener("click", function () {
+    welcomePage.classList.remove("active");
+    selectPage.classList.add("active");
+});
+
+board5x5.addEventListener("click", function () {
+    
+});
