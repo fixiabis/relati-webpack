4775d77a40b5cbce4a59afe159c9bb86cea87af6
fixiabis <benkao0000@gmail.com>
2018-07-18 22:39:36 +0800

    update

--- easy/js/RelatiGame.js
+++ easy/js/RelatiGame.js
@@ -1,5 +1,47 @@
 var RelatiGame = (function () {
     var symbol = "OX";
+    var symbolCreate = {
+        O: function (grid) {
+            return this.board.createViews([
+                {
+                    tag: "circle",
+                    attribute: {
+                        "stroke-width": 2,
+                        "cx": grid.x * 20 + 10,
+                        "cy": grid.y * 20 + 10,
+                        "r": 6,
+                        "stroke": "#dc143c",
+                        "fill": "none"
+                    }
+                }
+            ]);
+        },
+        X: function (grid) {
+            var startX = grid.x * 20 + 4;
+            var startY = grid.y * 20 + 4;
+            var endX = grid.x * 20 + 16;
+            var endY = grid.y * 20 + 16;
+            return this.board.createViews([
+                {
+                    tag: "path",
+                    attribute: {
+                        "stroke-width": 2,
+                        "d": "M " + startX + " " + startY + " L " + endX + " " + endY,
+                        "stroke": "#4169e1",
+                        "fill": "none"
+                    }
+                }, {
+                    tag: "path",
+                    attribute: {
+                        "stroke-width": 2,
+                        "d": "M " + startX + " " + endY + " L " + endX + " " + startY,
+                        "stroke": "#4169e1",
+                        "fill": "none"
+                    }
+                }
+            ]);
+        }
+    };
     function isRelati(grid, sym) {
         return grid.getGridsFromDir("O").filter(function (grid) { return grid && grid.symbol === sym; }).length > 0;
     }
@@ -10,6 +52,10 @@ var RelatiGame = (function () {
         if (this.turn < 2 || isRelati(grid, sym)) {
             grid.symbol = sym;
             this.turn++;
+            var views = symbolCreate[sym].bind(this)(grid);
+            for (var i = 0; i < views.length; i++) {
+                this.board.viewer.appendChild(views[i]);
+            }
         }
     }
     var RelatiGame = /** @class */ (function () {
@@ -18,7 +64,7 @@ var RelatiGame = (function () {
             board.viewer.addEventListener("click", function (event) {
                 var x = Math.floor(event.offsetX / 20), y = Math.floor(event.offsetY / 20);
                 gridSelected.bind(this)(board.grids[x][y]);
-            });
+            }.bind(this));
             this.turn = 0;
             this.board = board;
             window.addEventListener("resize", this.boardViewerResize.bind(this));
